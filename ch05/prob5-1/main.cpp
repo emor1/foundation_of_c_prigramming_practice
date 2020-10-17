a322aa995c95b3b4c179a74a0a461b33d0026fe1
[emori] <57478311+emor1@users.noreply.github.com>
2020-10-17 23:27:30 +0900

    5th day prob

---
+++ ch05/prob5-1/main.cpp
@@ -0,0 +1,11 @@
+#include "function.h"
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int m=3,n=1;
+    cout<<m<<"と"<<n<<"のうち最大のものは"<<Function::max(m,n)<<endl;
+    cout<<m<<"と"<<n<<"のうち最小のものは"<<Function::min(m,n)<<endl;
+    return 0;
+}
